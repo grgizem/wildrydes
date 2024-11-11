0c73f4cea972e1cdfa0f6d3fdd4b59d15aba3c15
Gizem Gur <gizem@sufle.io>
2024-11-11 17:51:13 +0300

    added api

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://imvx3y3br9.execute-api.us-east-1.amazonaws.com/prod',
     }
 }
